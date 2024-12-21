475f29912548fa6cbd5320248338dda8998ac3a8
zsyf452 <zsyfzs110@gmail.com>
2024-12-22 01:35:24 +0800

    堆的创建（最大堆

---
+++ 堆的创建（最大堆.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+int tree[100];
+int size = 1;
+
+inline int getLeft(int i){return i*2;}
+inline int getRight(int i){return i*2+1;}
+inline int getFather(int i){return i/2;}
+
+void exchange(int i,int t);
+void show();
+void up(int i);
+void init();
+
+int main()
+{
+
+    int arr[] = {23,43,12,2,1,23,3,4};
+    int len = sizeof(arr)/sizeof(arr[0]);
+    for(int i = 0;i<len;i++)
+    {
+        tree[size] = arr[i];
+        size++;
+    }
+    
+    // size++;
+    init();
+
+    show();
+    return 0;
+}
+
+
+
+
+void up(int i)
+{
+    int l = getLeft(i);
+    int r = getRight(i);
+
+    int t = 0;
+    if(l<size && tree[i] < tree[l])
+        t = l;
+    else
+        t = i;
+    
+    if(r < size && tree[t] < tree[r])
+        t = r;
+
+    if(t != i)
+    {
+        exchange(i,t);
+        up(t);
+    }    
+
+}
+
+void init()
+{
+    for (int i = size/2; i >= 1; i--)
+    {
+        up(i);
+    }
+    
+
+}
+
+
+void show()
+{
+    for (int i = 1; i < size; i++)
+    {
+        std::cout<<tree[i]<<' ';
+    }
+    
+}
+
+void exchange(int i,int t)
+{
+    int temp = tree[t];
+    tree[t] = tree[i];
+    tree[i] = temp;
+}
